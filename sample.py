2332b66fdf96721bac68b5fd67a56694e2bf0021
shinpaul14 <shinpaul14@khu.ac.kr>
2022-12-07 03:08:33 +0900

    fixed

--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
 
-import time
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import keras_cv
-from tensorflow import keras
-import matplotlib.pyplot as plt
+# import time
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# import keras_cv
+# from tensorflow import keras
+# import matplotlib.pyplot as plt
 import streamlit as st
 
 # model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
