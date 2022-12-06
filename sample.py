1e46e1b5535e70ba221720de5e241b61b38fd626
shinpaul14 <shinpaul14@khu.ac.kr>
2022-12-07 02:57:15 +0900

    fixed

--- sample.py
+++ sample.py
@@ -22,6 +22,12 @@ print(type(images))
 
 
 # plot_images(images)
+from datetime import datetime
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
 
 
 
