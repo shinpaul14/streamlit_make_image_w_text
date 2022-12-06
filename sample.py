9089ed26089b32a4ed79ca448914a7ad3c709f3c
shinpaul14 <shinpaul14@khu.ac.kr>
2022-12-07 02:49:12 +0900

    fixed

--- sample.py
+++ sample.py
@@ -5,20 +5,28 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import keras_cv
 from tensorflow import keras
 import matplotlib.pyplot as plt
-
+import streamlit as st
 
 model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
 
-images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=1)
 print(type(images))
 
-def plot_images(images):
-    plt.figure(figsize=(20, 20))
-    for i in range(len(images)):
-        ax = plt.subplot(1, len(images), i + 1)
-        plt.imshow(images[i])
-        plt.axis("off")
+# def plot_images(images):
+#     plt.figure(figsize=(20, 20))
+#     for i in range(len(images)):
+#         ax = plt.subplot(1, len(images), i + 1)
+#         plt.imshow(images[i])
+#         plt.axis("off")
 
 
-plot_images(images)
+# plot_images(images)
+
+
+
+from PIL import Image
+image = Image.fromarray(images)
+# image = Image.open('sunrise.jpg')
+
+st.image(image, caption='photograph of an astronaut riding a horse')
