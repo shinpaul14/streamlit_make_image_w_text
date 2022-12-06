751c8bb46266267d5ace03c1da39926ca10939d1
shinpaul14 <shinpaul14@khu.ac.kr>
2022-12-07 03:07:04 +0900

    fixed

--- sample.py
+++ sample.py
@@ -7,11 +7,11 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import streamlit as st
 
-model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+# model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
 
-images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=1)
-print(type(images))
+# images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=1)
+# print(type(images))
 
 # def plot_images(images):
 #     plt.figure(figsize=(20, 20))
@@ -31,8 +31,8 @@ st.write("Start time:", start_time)
 
 
 
-from PIL import Image
-image = Image.fromarray(images)
-# image = Image.open('sunrise.jpg')
+# from PIL import Image
+# image = Image.fromarray(images)
+# # image = Image.open('sunrise.jpg')
 
-st.image(image, caption='photograph of an astronaut riding a horse')
+# st.image(image, caption='photograph of an astronaut riding a horse')
