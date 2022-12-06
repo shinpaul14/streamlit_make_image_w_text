0337adcd76249d73f8c748945a2ee8d3435b3eca
shinpaul14 <shinpaul14@khu.ac.kr>
2022-12-07 01:25:01 +0900

    fixed

--- sample.py
+++ sample.py
@@ -1,18 +1,24 @@
-import streamlit as st
+
+import time
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
 
 
-# model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 
 
-# images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=1)
+images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+print(type(images))
 
-st.title('We Art')
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
 
 
-# from PIL import Image
-# image = Image.open(images)
-	
-# st.image(image)
-
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+plot_images(images)
